ce3a267112f0d22dba27b5917736b83dc5031a02
Priya.Sharma <priyasharma2702@gmail.com>
2024-04-11 18:16:04 +0530

    changed the build baseline

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -20,7 +20,7 @@ const Products = ({ products }: IProducts) => {
               <div className="group relative">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   <img
-                    src={`assets/products/shoes-${product.id}.png`}
+                    src={`src/assets/products/shoes-${product.id}.png`}
                     alt="Shoes"
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                   />
